0d3cde3af8cd1f1580395f2e2b95f684fbeb3599
Braian <braian.bergmann@gmail.com>
2023-01-25 16:24:27 +0200

    inital

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numbe 1 - 7");
+            int day = int.Parse(Console.ReadLine());
+            switch (day)
+            {
+
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Number is too big or small");
+                    break;
+            }
+        }
+    }
+}
